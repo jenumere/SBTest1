6b00e9bf08d506c6348057ed24459d0f97bb94a4
jenumere <clement@fastmail.com>
2022-06-03 11:28:47 +0200

    3 files modified

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ console.log('hello!');
 const icone = document.querySelector('i');
 const titre = document.querySelector('h1');
 icone.addEventListener('click', function () {
-  titre.innerText = 'testx';
+  if(titre.innerText === 'Titre'){
+    titre.innerText = 'Titre modifié';
+  }else{
+    titre.innerText = 'Titre';
+  }
+  
 });
