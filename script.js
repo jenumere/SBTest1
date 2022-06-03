cd3720f1999e522730dd796f920b37a920251e71
jenumere <clement@fastmail.com>
2022-06-03 13:43:08 +0200

    2 files modified, 1 added

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 console.log('hello!');
 const icone = document.querySelector('i');
 const titre = document.querySelector('h1');
+const lien = document.querySelector('a');
+console.log(lien);
 icone.addEventListener('click', function () {
-  if(titre.innerText === 'Titre'){
+  console.log('icone cliqué');
+  icone.classList.toggle('fa-smile');
+  if (titre.innerText === 'Titre') {
     titre.innerText = 'Titre modifié';
-  }else{
+    lien.classList.toggle('lien_masque');
+    icone.classList.toggle('icone_rouge');
+  } else {
     titre.innerText = 'Titre';
+    lien.classList.toggle('lien_masque');
+    icone.classList.toggle('icone_rouge');
   }
-  
 });
